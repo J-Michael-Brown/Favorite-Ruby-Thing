fb658c8d613da2c268803dfd35fa40a1389eaa84
Michael Brown and Rohan Moore <michael@>
2018-11-28 09:30:14 -0800

    work on project

---
+++ spec/item_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exception, false)
+
+describe('website', {:type => :feature}) do
+  it('stores the users favorite things') do
+    visit('/')
+    fill_in('name', :with => 'Dogs')
+    click_button('Go!')
+    expect(page).to have_content('Dogs')
+  end
+end
